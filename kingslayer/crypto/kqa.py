9b0479cf4565cb37e71e5d31a27db9d48f403aa9
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2023-09-11 08:42:02 -0500

    Create kqa.py

---
+++ kingslayer/crypto/kqa.py
@@ -0,0 +1,4 @@
+from kingslayer.q import QuantumRegister
+from kingslayer.q import QuantumGate
+from kingslayer.crypto.cryptocore import Cryptosystem
+from kingslayer.crypto.cryptocore import SymmetricAlgorithm
