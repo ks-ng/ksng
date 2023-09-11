7d421c94288c2b420b48c8c5fd5a471d654a8434
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2023-09-11 09:08:36 -0500

    Update kqa.py

--- kingslayer/crypto/kqa.py
+++ kingslayer/crypto/kqa.py
@@ -2,16 +2,21 @@ from kingslayer.q import QuantumRegister
 from kingslayer.q import QuantumGate
 from kingslayer.crypto.cryptocore import Cryptosystem
 from kingslayer.crypto.cryptocore import SymmetricAlgorithm
+from kingslayer.crypto.crux import CRUX
+
+from functools import cache
 
 @Cryptosystem.register("KQA")
 class KQA(SymmetricAlgorithm):
 
 	class Key:
 		def __init__(self, data: bytes):
-			self.data = data
+			self.data = self.original = data
 
 		def __bytes__(self) -> bytes:
 			return self.data
 
-		def permutation(self) -> list[list[int]]:
-			
+		@cache
+		def gates(self) -> list[QuantumGate]:
+			gates = []
+			for 
