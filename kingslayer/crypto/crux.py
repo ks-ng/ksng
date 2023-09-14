4a5e6a947dfdd0eaf45bfa0f9a68c194169e5492
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2023-09-14 08:23:29 -0500

    Update and rename crux.py to khais.py

--- kingslayer/crypto/crux.py
+++
@@ -1,7 +0,0 @@
-# Collision-Resistant Unified Exchange
-
-from kingslayer.crypto.cryptocore import Cryptosystem
-from kingslayer.crypto.cryptocore import HashingAlgorithm
-
-@Cryptosystem.register("CRUX")
-class CRUX(HashingAlgorithm):
---
+++ kingslayer/crypto/khais.py
@@ -0,0 +1,7 @@
+# Kingslayer Hashing Algorithm for Integrity and Security
+
+from kingslayer.crypto.cryptocore import Cryptosystem
+from kingslayer.crypto.cryptocore import HashingAlgorithm
+
+@Cryptosystem.register("KHAIS")
+class KHAIS(HashingAlgorithm):
