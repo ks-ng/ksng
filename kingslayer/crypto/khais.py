05eee7d7dbb032af27fac1dec685644a10e2cc27
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2023-09-14 08:47:32 -0500

    Update khais.py

--- kingslayer/crypto/khais.py
+++ kingslayer/crypto/khais.py
@@ -5,3 +5,12 @@ from kingslayer.crypto.cryptocore import HashingAlgorithm
 
 @Cryptosystem.register("KHAIS")
 class KHAIS(HashingAlgorithm):
+	def hash(self, data: bytes) -> bytes:
+		for _ in range(8):
+			output = []
+			for byte in data:
+				output.append(
+					pow(byte + sum(output), byte, 256)
+				)
+			data = bytes(output)
+		return data
