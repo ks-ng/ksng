a2b85e192ffbdefca70488d833402ee53e0c4416
k1n9514y3r <105938503+DragonlordNewb@users.noreply.github.com>
2023-11-01 10:09:49 -0500

    Update crypto.cpp

--- new/ksc/crypto.cpp
+++ new/ksc/crypto.cpp
@@ -25,7 +25,60 @@ class Encryption {
 
 		}
 
-		template 
-		virtual int* encrypt(unsigned char plaintext[]
+		virtual unsigned char* encrypt(unsigned char plaintext[], Key key, const int length) = 0;
+		virtual unsigned char* decrypt(unsigned char ciphertext[], Key key, const int length) = 0;
 
+}
+
+// Hashing - verifying data
+class Hash {
+
+	public:
+
+		enum saltMode { prefix, suffix, xor };
+
+		virtual unsigned char* hash(unsigned char text[], const int length) = 0;
+
+		unsigned char* saltedHash(
+				unsigned char text[], 
+				const int textLength,
+				unsigned char salt[], 
+				const int saltLength,
+				saltMode mode ) {
+
+			if (mode == xor) {
+				const int combinedLength = textLength;
+				unsigned char combined[textlength];
+				for (int i = 0; i < textLength; i++) {
+					combined[i] = text[i] ^ salt[i];
+				}
+			} else if (mode == prefix) {
+				const int combinedLength = textLength + saltLength;
+				unsigned char combined[combinedLength];
+				int i = 0;
+				for (int j = 0; j < saltLength; j++) {
+					combined[i] = salt[j];
+					i++;
+				}
+				for (int j = 0; j < textLength; j++) {
+					combined[i] = salt[j];
+					i++;
+				}
+			} else if (mode == suffix) {
+				const int combinedLength = textLength + saltLength;
+				unsigned char combined[combinedLength];
+				int i = 0;
+				for (int j = 0; j < textLength; j++) {
+					combined[i] = text[j];
+					i++;
+				}
+				for (int j = 0; j < saltLength; j++) {
+					combined[i] = salt[j];
+					i++;
+				}
+			}
+			
+			return hash(combined, combinedLength);
+
+		}
 }
