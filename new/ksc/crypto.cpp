8d7ebb046061b16653a376d85a4989a048852de8
k1n9514y3r <105938503+DragonlordNewb@users.noreply.github.com>
2023-11-01 09:21:01 -0500

    Create crypto.cpp

---
+++ new/ksc/crypto.cpp
@@ -0,0 +1,28 @@
+#pragma once
+
+// Encryption - securing data
+
+class Encryption {
+
+	public:
+
+		template <const int len>
+		class Key {
+
+			private:
+
+				unsigned char keyData[len];
+
+			public:
+
+				const int length = len;
+
+				Key(unsigned char data[len]) {
+					for (int i = 0; i < length; i++) {
+						keyData[i] = data[i];
+					}
+				}
+
+		}
+
+}
