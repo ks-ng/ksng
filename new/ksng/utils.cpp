d6fd388cb870359d7f15f44f4e9cf9440f127ef6
k1n9514y3r <105938503+DragonlordNewb@users.noreply.github.com>
2023-11-02 08:02:27 -0500

    gtewgtwgt

--- new/ksng/utils.cpp
+++ new/ksng/utils.cpp
@@ -12,6 +12,13 @@
 
 using namespace std;
 
+const char* HEX_ALPHABET[16] = {
+	"0", "1", "2", "3",
+	"4", "5", "6", "7",
+	"8", "9", "A", "B",
+	"C", "D", "E", "F"
+};
+
 class Bytestring {
 
 	public:
@@ -54,6 +61,23 @@ class Bytestring {
 			}
 
 			return result
+		}
+
+		void printHex() {
+			const int hexStringLength = (2 * length);
+			const char* hexadecimal[hexStringLength];
+			unsigned char byte;
+			unsigned char first;
+			unsigned char last;
+
+			for (int i = 0; i < length; i++) {
+				byte = data[i];
+				first = (byte & 0xF0) >> 4;
+				last = (byte & 0x0F);
+
+				cout << HEX_ALPHABET[first];
+				cout << HEX_ALPHABET[last];
+			}
 		}
 
 };
