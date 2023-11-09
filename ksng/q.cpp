b506f19425c3be413d79e8b67a81d4e5c497e94e
k1n9514y3r <105938503+DragonlordNewb@users.noreply.github.com>
2023-11-09 11:35:35 -0600

    Update q.cpp

--- ksng/q.cpp
+++ ksng/q.cpp
@@ -1,1 +1,22 @@
 #include "utils.cpp"
+
+template <int bitlength>
+class Ket {
+
+	public:
+
+		long long length = pow(2, bitlength);
+
+		complex<double> amplitudes[size];
+
+		QuantumState(long long value) {
+			for (long long i = 0; i < size; i++) {
+				if (i == value) {
+					amplitudes[i] = complex<double>(1.0, 0.0);
+				} else {
+					amplitudes[i] = complex<double>(0.0, 0.0);
+				}
+			}
+		}
+
+}
