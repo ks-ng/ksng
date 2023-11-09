b79566e79ed8bc057d202544c3d3700c8083a81e
k1n9514y3r <105938503+DragonlordNewb@users.noreply.github.com>
2023-11-09 11:54:01 -0600

    Update q.cpp

--- ksng/q.cpp
+++ ksng/q.cpp
@@ -1,22 +1,69 @@
 #include "utils.cpp"
 
-template <int bitlength>
 class Ket {
+
+	/*
+ 	Kets represent quantum superpositions of
+  	binary numbers.
+
+	Each ket is associated with a decimal number
+ 	which is encoded by its qubits. Applying a bra
+  	to a ket returns the probability of the bits
+   	acquired from measuring the qubits encoding
+    	the bra's decimal number.
+	*/
 
 	public:
 
-		long long length = pow(2, bitlength);
+		int bitlength;
+		int size;
 
-		complex<double> amplitudes[size];
-
-		QuantumState(long long value) {
+		QuantumState(long long value, int bitlength_) {
+			bitlength = bitlength_;
+			size = pow(2, bitlength)
+			complex<double> amps[size];
+			
 			for (long long i = 0; i < size; i++) {
 				if (i == value) {
-					amplitudes[i] = complex<double>(1.0, 0.0);
+					amps[i] = complex<double>(1.0, 0.0);
 				} else {
-					amplitudes[i] = complex<double>(0.0, 0.0);
+					amps[i] = complex<double>(0.0, 0.0);
 				}
 			}
 		}
+
+		double amplitude(int value) {
+			return amps[value];
+		}
+
+		double probability(int value) {
+			return pow(abs(amps[value]), 2);
+		}
+
+};
+
+class Bra {
+
+	public:
+
+		long long value;
+
+		Bra(long long value_) {
+			value = value_;
+		}
+
+		complex<double> operator|(Ket state) {
+			return state.amplitude(value);
+		}
+
+};
+
+complex<double> innerProduct(Bra bra, Ket ket) {
+	return /*<*/ bra | ket /*>*/ ;
+}
+
+class OuterProduct {
+
+
 
 }
