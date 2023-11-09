705adb0db507f03c0a0ea084168382f66853bfb8
k1n9514y3r <105938503+DragonlordNewb@users.noreply.github.com>
2023-11-09 12:36:22 -0600

    Update q.cpp

--- ksng/q.cpp
+++ ksng/q.cpp
@@ -40,6 +40,16 @@ class Ket {
 			return pow(abs(amps[value]), 2);
 		}
 
+		Ket operator*(Ket other) {
+			Ket result(-1, bitlength * other.bitlength);
+			for (long long i = 0; i < size; i++) {
+				for (long long j = 0; j < other.size; j++) {
+					result.amps = amps[i] * amps[j];
+				}
+			}
+			return result;
+		}
+
 };
 
 class Bra {
