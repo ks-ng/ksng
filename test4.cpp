371d0cfcd95bcf61aeaaa2250d7d7fa762f3082f
k1n9514y3r <105938503+DragonlordNewb@users.noreply.github.com>
2023-11-20 14:51:40 +0000

    hnnnmnm

--- test4.cpp
+++ test4.cpp
@@ -1,27 +1,7 @@
 #include "ksng/crypto.cpp"
 
-class MyCipher: public basic::Cipher {
-
-	public:
-
-		Bytestring encrypt(Bytestring plaintext, basic::Key key) {
-			key.reveal();
-			Bytestring result = Bytestring(plaintext, key.accessData());
-			key.hide();
-			return result;
-		}
-
-		Bytestring decrypt(Bytestring ciphertext, basic::Key key) {
-			key.reveal();
-			Bytestring result = Bytestring(ciphertext, key.accessData());
-			key.hide();
-			return result;
-		}
-
-};
-
 int main() {
-	MyCipher cipher;
+	impl::OneTimePad cipher;
 	unsigned char raw[5] = {1, 2, 3, 4, 5};
 	Bytestring rkd(raw, 5);
 	basic::Key key(&rkd);
