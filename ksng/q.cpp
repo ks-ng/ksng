4be7a98dac67868243f730a5fe7625628fe494bd
DragonlordNewb <105938503+DragonlordNewb@users.noreply.github.com>
2023-11-23 16:57:09 -0600

    rgwwgr

--- ksng/q.cpp
+++ ksng/q.cpp
@@ -1,140 +1,49 @@
-#include "utils.cpp"
+#pragma once
 
 #include <complex>
+#include <cmath>
 
 using namespace std;
 
-class Matrix {
+#define COMPLEX complex<double>
 
-	public: 
-	
-		complex<double>** data;
+class QVec {
+
+	public:
+
 		int size;
+		COMPLEX* elements;
+
+		QVec(int size): size(size), elements(new COMPLEX[size]) {}
+		QVec(int size, COMPLEX data[]): size(size), elements(data) {}
+
+		COMPLEX operator[](int index) {
+			return elements[index];
+		}
+
+};
+
+class QMatrix {
+
+	public:
+
+		int size;
+		QVec* rows;
+
+		QMatrix(int size): size(size), rows(new QVec[size]) {
+			for (int i = 0; i < size; i++) {
+				rows[i] = QVec(size);
+			}
+		}
 		
-		Matrix() {}
+		QMatrix(int size, QVec data[]): size(size), rows(data) {}
 
-		Matrix(int size_) {
-			size = size_;
-			data = new std::complex<double>*[size];
-			for (int i = 0; i < size; ++i) {
-				data[i] = new std::complex<double>[size];
-			}
+		QVec operator[](int index) {
+			return rows[index]
 		}
 
-		~Matrix() {
-			for (int i = 0; i < size; ++i) {
-				delete[] data[i];
-			}
-			delete[] data;
-		}
-
-};
-
-class Ket {
-
-	public:
-
-		int stateCount;
-		complex<double>* amps;
-
-		Ket(const int stateCount_) {
-			int stateCount = stateCount_;
-			amps = new complex<double>[stateCount];
-			double frac = 1 / sqrt(stateCount);
-			for (int i = 0; i < stateCount; i++) {
-				amps[i] = complex<double>(frac, 0);
-			}
-		}
-
-		double netProbability() {
-			double ampsum = 0.0;
-			for (int i = 0; i < stateCount; i++) {
-				ampsum += pow(abs(amps[i]), 2);
-			}
-			return ampsum;
-		}
-
-		void normalize() {
-			complex<double> np(netProbability(), 0.0);
-			for (int i = 0; i < stateCount; i++) {
-				amps[i] = amps[i] / np;
-			}
-		}
-
-		Ket entangle(Ket other) {
-			Ket result(stateCount * other.stateCount);
-			int index = 0;
-			for (int i = 0; i < stateCount; i++) {
-				for (int j = 0; j < other.stateCount; j++) {
-					result.amps[index] = amps[i] * other.amps[j];
-					index++;
-				}
-			}
-			return result;
-		}
-
-		Ket operator+(Ket other) {
-			Ket result(stateCount);
-			for (int i = 0; i < stateCount; i++) {
-				result.amps[i] = amps[i] + other.amps[i];
-			}
-			return result;
-		}
-
-		Ket operator-(Ket other) {
-			Ket result(stateCount);
-			for (int i = 0; i < stateCount; i++) {
-				result.amps[i] = amps[i] - other.amps[i];
-			}
-			return result;
-		}
-
-		Ket operator*(Ket other) {
-			Ket result(stateCount);
-			for (int i = 0; i < stateCount; i++) {
-				result.amps[i] = amps[i] * other.amps[i];
-			}
-			return result;
-		}
-
-};
-
-class Bra {
-
-	public:
-
-		int state;
-
-		Bra(int state_) {
-			state = state_;
-		}
-
-		complex<double> operator|(Ket ket) {
-			return ket.amps[state];
-		}
-
-};
-
-class Operator {
-
-	public:
-
-		Matrix* matrixptr;
-
-		Operator(Matrix* matrix_) {
-			matrixptr = matrix_;
-		}
-
-		Matrix matrix() {
-			return *matrixptr;
-		}
-
-		Ket operator|(Ket ket) {
-			Ket result(ket.stateCount);
-
-			// Apply the operator to the ket
-
-			return result;
+		void make(QVec a, QVec b) {
+			for (int i = 0;)
 		}
 
 };
---
+++ old/q.cpp
@@ -0,0 +1,140 @@
+#include "utils.cpp"
+
+#include <complex>
+
+using namespace std;
+
+class Matrix {
+
+	public: 
+	
+		complex<double>** data;
+		int size;
+		
+		Matrix() {}
+
+		Matrix(int size_) {
+			size = size_;
+			data = new std::complex<double>*[size];
+			for (int i = 0; i < size; ++i) {
+				data[i] = new std::complex<double>[size];
+			}
+		}
+
+		~Matrix() {
+			for (int i = 0; i < size; ++i) {
+				delete[] data[i];
+			}
+			delete[] data;
+		}
+
+};
+
+class Ket {
+
+	public:
+
+		int stateCount;
+		complex<double>* amps;
+
+		Ket(const int stateCount_) {
+			int stateCount = stateCount_;
+			amps = new complex<double>[stateCount];
+			double frac = 1 / sqrt(stateCount);
+			for (int i = 0; i < stateCount; i++) {
+				amps[i] = complex<double>(frac, 0);
+			}
+		}
+
+		double netProbability() {
+			double ampsum = 0.0;
+			for (int i = 0; i < stateCount; i++) {
+				ampsum += pow(abs(amps[i]), 2);
+			}
+			return ampsum;
+		}
+
+		void normalize() {
+			complex<double> np(netProbability(), 0.0);
+			for (int i = 0; i < stateCount; i++) {
+				amps[i] = amps[i] / np;
+			}
+		}
+
+		Ket entangle(Ket other) {
+			Ket result(stateCount * other.stateCount);
+			int index = 0;
+			for (int i = 0; i < stateCount; i++) {
+				for (int j = 0; j < other.stateCount; j++) {
+					result.amps[index] = amps[i] * other.amps[j];
+					index++;
+				}
+			}
+			return result;
+		}
+
+		Ket operator+(Ket other) {
+			Ket result(stateCount);
+			for (int i = 0; i < stateCount; i++) {
+				result.amps[i] = amps[i] + other.amps[i];
+			}
+			return result;
+		}
+
+		Ket operator-(Ket other) {
+			Ket result(stateCount);
+			for (int i = 0; i < stateCount; i++) {
+				result.amps[i] = amps[i] - other.amps[i];
+			}
+			return result;
+		}
+
+		Ket operator*(Ket other) {
+			Ket result(stateCount);
+			for (int i = 0; i < stateCount; i++) {
+				result.amps[i] = amps[i] * other.amps[i];
+			}
+			return result;
+		}
+
+};
+
+class Bra {
+
+	public:
+
+		int state;
+
+		Bra(int state_) {
+			state = state_;
+		}
+
+		complex<double> operator|(Ket ket) {
+			return ket.amps[state];
+		}
+
+};
+
+class Operator {
+
+	public:
+
+		Matrix* matrixptr;
+
+		Operator(Matrix* matrix_) {
+			matrixptr = matrix_;
+		}
+
+		Matrix matrix() {
+			return *matrixptr;
+		}
+
+		Ket operator|(Ket ket) {
+			Ket result(ket.stateCount);
+
+			// Apply the operator to the ket
+
+			return result;
+		}
+
+};
