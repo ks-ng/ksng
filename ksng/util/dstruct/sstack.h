dd3cbdcaa1d6e61001b0adec0bd33a0bf6b8763c
k1n9514y3r <105938503+DragonlordNewb@users.noreply.github.com>
2024-01-24 12:29:23 -0600

    Update sstack.h

--- ksng/util/dstruct/sstack.h
+++ ksng/util/dstruct/sstack.h
@@ -10,6 +10,23 @@ namespace sstack {
 			sll::SecureLinkedList<T> stk;
 			bool locked;
 
+		public:
+
+			// Security
+
+			void reveal() { locked = false; }
+			void hide() { locked = true; }
+			void securityCheck() { 
+				if (locked) { 
+					notif::security("invalid attempt to access to secure stack", ALERT);
+				} 
+			}
+
+			// Access
+
+			void push(T value) { stk.add(sll::SLLE<T>(value, false), stk.getLength() - 1); }
+			T pop() { int x = stk.getLength() - 1; T r = stk.getValue(x); stk.remove(x); return r; }
+
 	};
 
 };
