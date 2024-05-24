a139194f28d33e128f6996120e484e54cad47281
ks-ng <168195294+ks-ng@users.noreply.github.com>
2024-05-24 10:48:30 -0500

    qubits and circuits!

---
+++ ksng/qce/circuit.h
@@ -0,0 +1,43 @@
+#pragma once
+#include "qudata.h"
+
+namespace circuit {
+
+    class QuantumGate {
+
+        private:
+
+            sda::Array<int> indices;
+            qcore::QO operation;
+
+        public:
+
+            QuantumCircuitComponent(int index, qcore::QuantumOperator op) {
+                indices = sda:Array<int>({index});
+                operation = op;
+            }
+
+            sda::Array<int> getIndices() { return indices; }
+            int getIndex(int i) { return indices.get(i); }
+            qcore::QO getOperator() { return operation; }
+
+    };
+
+    class QuantumCircuit: public sda::Array<QuantumGate> {
+
+        public:
+
+            QuantumCircuit(initializer_list<QuantumGate> initList) {
+				length = initList.size();
+				elements = new QuantumGate[length];
+				locked = false;
+				securityLevel = ALERT;
+				int i = 0;
+				for (const auto& val : initList) {
+					elements[i++] = val;
+				}
+			}
+
+    };
+
+};
--- ksng/qce/qudata.h
+++ ksng/qce/qudata.h
@@ -1,20 +1,61 @@
+#pragma once
 #include "qcore.h"
 
 namespace qudata {
 
-	enum DataState { Separated, Combined };
+	qcore::QuantumState mkqb(int v) {
+		if (v == 0) {
+			return qcore::QuantumState({1, 0});
+		} else (
+			return qcore::QuantumState({0, 1});
+		)
+	}
 
-	class Qubits {
-
-		private:
-
-			sda::Array<qcore::QuantumState> qubits;
-			qcore::QuantumState combinedState;
-			DataState dataState;
+	class Qubits: public sda::Array<qcore::QuantumState> {
 
 		public:
 
-			
+			Qubits(initializer_list<qcore::QS> initList) {
+				length = initList.size();
+				elements = new qcore::QS[length];
+				locked = false;
+				securityLevel = ALERT;
+				int i = 0;
+				for (const auto& val : initList) {
+					elements[i++] = mkqb(val);
+				}
+			}
+
+			Qubits(int x, int mbl = 0) {
+				int l2l = max(ceil(log2(x)), mbl);
+				initialize(l2l)
+				for (int i = 0; i < l2l; i++) {
+					set(i, mkqb(x & (1 << i)));
+				}
+			}
+
+			void applyOperator(int index, qcore::QuantumOperator op) {
+				set(index, op | get(index));
+			}
+
+			void applyDoubleOperator(int a, int b, qcore::QO op) {
+				// slightly more optimal than applyMultipleOperator
+				qcore::QS state = get(a) * get(b);
+				sda::Array<qcore::QS> data = (op | state).extractQuantumData();
+				set(a, data.get(0));
+				set(b, data.get(0));
+			}
+
+			void applyMultipleOperator(sda::Array<int> indices, qcore::QO op) {
+				qcore::QS state = get(indices.get(0));
+				for (int i = 1; i < indices.getLength(); i++) {
+					state = state * get(indices.get(i));
+				}
+				sda::Array<qcore::QS> data = (op | state).extractQuantumData();
+				for (int i = 0; i < data.getLength(); i++) {
+					set(indices.get(i), data.get(i));
+				}
+			}
 
 	};
 
